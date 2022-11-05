0809fcc28ac8dd7a42196b878f12f6ff086e8c1a
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-11-05 13:02:40 -0400

    Initial commit

---
+++ src/main/java/com/coderscampus/ogulcanassignment9/OgulcanAssignment9Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.ogulcanassignment9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgulcanAssignment9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgulcanAssignment9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/ogulcanassignment9/OgulcanAssignment9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.ogulcanassignment9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgulcanAssignment9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
