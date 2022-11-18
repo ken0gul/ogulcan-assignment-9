00ea741f75ded0c43ef014cf8c8a40e3714ed8ef
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-11-18 10:23:10 -0500

    Remove Autowiring for fileService

--- src/main/java/com/coderscampus/ogulcanassignment9/web/FileController.java
+++ src/main/java/com/coderscampus/ogulcanassignment9/web/FileController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,46 +13,50 @@ import com.coderscampus.ogulcanassignment9.service.FileService;
 @RestController
 public class FileController {
 
-	@Autowired
-	private FileService fileService;
-
+//	@Autowired
+//	private FileService fileService;
+	
+	
+		
+	
+	
 	@GetMapping("/all-recipes")
 	public List<Recipe> getAllRecipes() throws IOException {
-		 List<Recipe> list =  fileService.readFile("recipe.txt");
-		 return list;
+		FileService fileService = new FileService();
+		 
 		
-		
-		
-	}
+		 return fileService.readFile("recipe.txt");
+}
 	
 	
 	@GetMapping("/gluten-free") 
 	public List<Recipe>  getGlutenFree() throws Exception {
-		List<Recipe> list = fileService.readFile("recipe.txt");
-		return list.stream()
+		FileService fileService = new FileService();
+		return fileService.readFile("recipe.txt").stream()
 			.filter(item -> item.getGlutenFree())
 			.collect(Collectors.toList());
 	}
 	
 	@GetMapping("/vegan-and-gluten-free")
 	public List<Recipe>  getVeganAndGlutenFree() throws Exception {
-		List<Recipe> list = fileService.readFile("recipe.txt");
-		return list.stream()
-			.filter(item -> item.getGlutenFree())
-			.filter(i -> i.getVegan())
-			.collect(Collectors.toList());
+		
+		FileService fileService = new FileService();
+			
+			return  fileService.readFile("recipe.txt").stream()
+					.filter(item -> item.getGlutenFree())
+					.filter(i -> i.getVegan())
+					.collect(Collectors.toList());
 	}
 	@GetMapping("/vegetarian")
 	public List<Recipe>  getVegetarian() throws Exception {
-		List<Recipe> list = fileService.readFile("recipe.txt");
-		return list.stream()
-			.filter(item -> item.getVegetarian())
+		FileService fileService = new FileService();
+		return fileService.readFile("recipe.txt").stream().filter(item -> item.getVegetarian())
 			.collect(Collectors.toList());
 	}
 	@GetMapping("/vegan")
 	public List<Recipe>  getVegan() throws Exception {
-		List<Recipe> list = fileService.readFile("recipe.txt");
-		return list.stream()
+		FileService fileService = new FileService();
+		return fileService.readFile("recipe.txt").stream()
 			.filter(item -> item.getVegan())
 			.collect(Collectors.toList());
 	}
