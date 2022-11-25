b4fa9cb9277f548cac709a4a1ffc07f98c596473
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-11-25 10:25:49 -0500

    Resolve the conflicts regarding the revert

--- src/main/java/com/coderscampus/ogulcanassignment9/service/RecipeService.java
+++ src/main/java/com/coderscampus/ogulcanassignment9/service/RecipeService.java
@@ -13,15 +13,14 @@ import com.coderscampus.ogulcanassignment9.domain.Recipe;
 @Service
 public class RecipeService {
 
-	
 	@Autowired
 	private FileService fileService;
-	
+
 	private List<Recipe> recipes = new ArrayList<>();
-	
+
 	public List<Recipe> getAllRecipes() {
-		
-		if(CollectionUtils.isEmpty(recipes)) {
+
+		if (CollectionUtils.isEmpty(recipes)) {
 			try {
 				recipes = fileService.readFile("recipe.txt");
 			} catch (IOException e) {
@@ -29,8 +28,8 @@ public class RecipeService {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return recipes;
 	}
-	
+
 }
